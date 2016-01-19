042f4beed4b10a2ed2f93f61b23a97df460ee7c3
Arkangel0x7C5 <arkangelhacket@gmail.com>
2016-01-19 20:19:52 +0100

    agregado soporte de log

--- claptrap.py
+++ claptrap.py
@@ -4,9 +4,14 @@ import datetime
 import re
 import conf
 import commands
+import json
+import logging
+import os
+import sys
 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-
+logging.basicConfig(filename='debug.log',level=logging.DEBUG)
 bot = telepot.Bot(conf.botID)
 botName = bot.getMe()['username']
 frases = ["no tengo frases, que planeas que diga?"]
@@ -18,6 +23,7 @@ btcPriceRegex = re.compile('\/btc([Pp]rice)?(@'+botName+')? *$')
 
 
 def handle(msg):
+	logging.debug(json.dumps(msg)+"\n")
 	if not msg.has_key("text") and not msg.has_key("chat"):
 		return
 	chat_id = msg['chat']['id']
