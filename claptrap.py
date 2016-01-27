f50c8c2eb515c51b7b11d267c26fb8fee62bbbac
Arkangel0x7C5 <arkangelhacket@gmail.com>
2016-01-27 14:48:45 +0100

    cambio de libreria de telegram

--- claptrap.py
+++ claptrap.py
@@ -1,5 +1,5 @@
 import time
-import telepot
+import telegram
 import re
 import conf
 import commands
@@ -11,27 +11,30 @@ import sys
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 logging.basicConfig(filename='debug.log',level=logging.DEBUG)
-bot = telepot.Bot(conf.botID)
+bot = telegram.Bot(conf.botID)
 
 commands.init(bot.getMe()['username'])
 
 
-def handle(msg):
+def handle(bot,msg):
 	logging.debug(json.dumps(msg)+"\n")
 	#if not msg.has_key("text") and not msg.has_key("chat"):
 	if "text" not in msg and "chat" not in msg:
 		return
+	logging.debug("test\n")
 	chat_id = msg['chat']['id']
 	text	= msg['text']
 	
-	print("Mensaje recibido: ", text)
+	print("Mensaje recibido: "+ text)
 	
 	for comando in commands.list:
 		if comando[0].match(text):
 			bot.sendMessage(chat_id,comando[1]())
 			return
+	#bot.sendMessage(chat_id,"test")
 
-bot.notifyOnMessage(callback=handle, run_forever=True)
+#bot.notifyOnMessage(callback=handle, run_forever=True)
+bot.startPolling(handle)
 
-while(1):
-	time.sleep(1)
+#while(1):
+#	time.sleep(10)
